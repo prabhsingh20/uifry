06d40d9771c0f8da8f354bcef6ef8118b2bac95a
Prabh ‪‪ <singh20prabh@gmail.com>
2024-06-03 01:35:32 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        theme: "#FF5555",
+        black: "#000",
+        white: "#fff",
+      },
+    },
+  },
+  plugins: [],
+};
